ae0a6b2b68724bf0d307da115b94217d865b8055
pratiksinghal <pratiksinghal48@gmail.com>
2017-12-04 17:08:08 +0530

    intial project commit

---
+++ src/main/java/gen/api/mvc/App.java
@@ -0,0 +1,13 @@
+package gen.api.mvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
